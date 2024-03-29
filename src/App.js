3c95e18fcea7354325611b1b0c2a485627658192
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2024-03-29 10:26:30 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
+import { SafeAppProvider } from '@safe-global/safe-apps-provider';
+import { useMemo } from 'react';
 
 function App() {
+  const { sdk, safe } = useSafeAppsSDK();
+  const web3Provider = useMemo(() => new ethers.BrowserProvider(new SafeAppProvider(safe, sdk)), [sdk, safe]);
+  console.log("🚀 ~ App ~ web3Provider:", web3Provider)
+
   return (
     <div className="App">
       <header className="App-header">
